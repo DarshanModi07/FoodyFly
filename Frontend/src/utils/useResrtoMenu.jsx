3079bb661993d1dc70737075811bd2757fdb0509
Darshan <darshanmodi016@gmail.com>
2026-03-17 14:23:37 +0530

    Fix ID inn URL

--- Frontend/src/utils/useResrtoMenu.jsx
+++ Frontend/src/utils/useResrtoMenu.jsx
@@ -1,10 +1,10 @@
 import {useState,useEffect} from "react"
 import axios from "axios"
-const BASE_URL = import.meta.env.BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const useRestroMenu = (id) => {
     
-    const index = parseInt(id);
+    const index = id;
     const num = index
 
     const[ data, setData] = useState([]);
